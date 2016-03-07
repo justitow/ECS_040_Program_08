4a5b7723786367456e37a14d83e2257c923d892a
justitow <justitow@gmail.com>
2016-03-06 17:44:21 -0800

    tranfered davis's files

--- main.cpp
+++ main.cpp
@@ -1,9 +1,31 @@
-#include <iostream>
+#include <fstream>
+#include "pager.h"
+#include "indexpager.h"
 
 using namespace std;
 
-int main()
+
+int main(int argc, char* argv[])
 {
-  cout << "Hello, World!" << endl;
+  Pager pager, *ptr;
+  IndexPager indexer;
+  char c;
+
+  ifstream inf(argv[1]);
+  if(argv[2][0] == '1')
+    ptr = &pager;
+  else
+    ptr = (Pager*) &indexer;
+
+  ptr->read(inf);
+  inf.close();
+
+  ptr->ProcessKey('b');  // Causes initial write
+  do{
+    cin >> c;
+    ptr->ProcessKey(c);
+  } while (c != 'q');
+
   return 0;
 }
+//---------------------------------------------------------------------------
